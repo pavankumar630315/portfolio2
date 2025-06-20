a37290a522980a9edefa0d25e56df763297973ee
pavankumar630315 <gorlapavan12345@gmail.com>
2025-06-20 12:35:44 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-3xl font-bold text-blue-600">Pavan Kumar Gorla</h1>
+      <p className="mt-4">Welcome to my portfolio powered by React and Tailwind CSS!</p>
+    </div>
+  );
+}
+
+export default App;
